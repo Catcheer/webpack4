093095ea567feb87e594b7b05142fb7baa5bab6f
zhangchuangye <1072491557@qq.com>
2018-09-12 10:32:56 +0800

    HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 
 const path=require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports={
     entry:{
@@ -10,5 +11,10 @@ module.exports={
         path:path.resolve(__dirname,'./dist'),
         filename:'[name].bundle.js'
     },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title:'Output Management'
+        })
+    ]
 
 }
